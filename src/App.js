6808acb889134fb0d86d3ad9a7af4acb38047ff3
Laura Kesel <laura.kesel@gmail.com>
2022-05-23 21:28:58 +0200

    refactor design and add props to cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Header from "./Header";
-import Footer from "./Footer";
-import Homepage from "./Homepage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
---
+++ src/components/Homepage.js
@@ -0,0 +1,26 @@
+import "../App.css";
+import Card from "./Card";
+
+function Homepage() {
+  return (
+    <main className="App-main">
+      <Card
+        name="Alien Morty"
+        imgURL={"https://rickandmortyapi.com/api/character/avatar/14.jpeg"}
+        isBookmarked={false}
+      />
+      <Card
+        name="Pizza-person"
+        imgURL="https://rickandmortyapi.com/api/character/avatar/425.jpeg"
+        isBookmarked={true}
+      />
+      <Card
+        name="Doopidoo"
+        imgURL="https://rickandmortyapi.com/api/character/avatar/433.jpeg"
+        isBookmarked={false}
+      />
+    </main>
+  );
+}
+
+export default Homepage;
